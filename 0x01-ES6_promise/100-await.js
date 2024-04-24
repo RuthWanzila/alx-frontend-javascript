05b26b5b162036480d5d02c334c773bf46d10424
RuthWanzila <wanzilaruth533@gmail.com>
2024-04-24 12:39:55 -0700

    await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,21 +1,14 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
-async function asyncUploadUser() {
+const asyncUploadUser = async () => {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
 
-    return {
-      photo,
-      user,
-    };
+    return { photo, user };
   } catch (error) {
-    console.error(error);
-    return {
-      photo: null,
-      user: null,
-    };
+    return { photo: null, user: null };
   }
-}
+};
 
 export default asyncUploadUser;
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,10 @@
-import handleProfileSignup from "./3-all";
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
-    .catch(() => {
-      console.log("Signup system offline");
-    });
-}
 
-handleProfileSignup();
+    .catch(() => console.log('Signup system offline'));
+}
